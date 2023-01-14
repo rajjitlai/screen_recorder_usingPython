0a1b5e2e3357d6c6dd8993b13132d50019a46199
Rajjit <2.0lairaj@gmail.com>
2023-01-14 13:26:25 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from PIL import ImageGrab
+from win32api import GetSystemMetrics
+import numpy as nV
+import datetime
+import cv2
+
+print('Created by Rajjit Laishram'[::1])
+
+vidWidth = GetSystemMetrics(0)
+vidHeight = GetSystemMetrics(1)
+
+file_time_format = datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+saved_file = f'{file_time_format}.mp4'
+fileName = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+recorded_video = cv2.VideoWriter(saved_file, fileName, 30.0, (vidWidth, vidHeight))
+
+for_webCam = cv2.VideoCapture(0)
+
+while True:
+          vid = ImageGrab.grab(bbox=(0,0, vidWidth, vidHeight))
+          vid_nV = nV.array(vid)
+          vid_res = cv2.cvtColor(vid_nV, cv2.COLOR_BGR2RGB)
+          _, wC_frame = for_webCam.read()
+          wC_height, wC_width, _ = wC_frame.shape
+          vid_res[0:wC_height, 0: wC_width, :] = wC_frame[0: wC_height, 0:wC_width, :]
+          
+          cv2.imshow('RJs Screen Recorder', vid_res)
+          # cv2.imshow('RJs WebCam Recorder', wC_frame)
+          
+          recorded_video.write(vid_res)
+          
+          if cv2.waitKey(10) == ord('x'):
+                    break
+          
