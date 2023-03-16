e76d10a34fb82a22eaad3a2e63fc695d99234ef6
Rajjit <2.0lairaj@gmail.com>
2023-03-16 21:19:26 +0530

    Updated v1

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # pip install PILLOW and then import ImageGrab
 from PIL import ImageGrab
-# pip install win32api and then import GetSystemMetrics
+# pip install pywin32 and then import GetSystemMetrics
 from win32api import GetSystemMetrics
-# pip install cv2 and then import cv2
+# pip install cv and then import cv2
 import cv2
 # import the built in modules
 import numpy as nV
@@ -18,15 +18,15 @@ saved_file = f'{file_time_format}.mp4'
 fileName = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 recorded_video = cv2.VideoWriter(saved_file, fileName, 30.0, (vidWidth, vidHeight))
 
-for_webCam = cv2.VideoCapture(0)
+# for_webCam = cv2.VideoCapture(0)
 
 while True:
           vid = ImageGrab.grab(bbox=(0,0, vidWidth, vidHeight))
           vid_nV = nV.array(vid)
           vid_res = cv2.cvtColor(vid_nV, cv2.COLOR_BGR2RGB)
-          _, wC_frame = for_webCam.read()
-          wC_height, wC_width, _ = wC_frame.shape
-          vid_res[0:wC_height, 0: wC_width, :] = wC_frame[0: wC_height, 0:wC_width, :]
+          # _, wC_frame = for_webCam.read()
+          # wC_height, wC_width, _ = wC_frame.shape
+          # vid_res[0:wC_height, 0: wC_width, :] = wC_frame[0: wC_height, 0:wC_width, :]
           
           cv2.imshow('RJs Screen Recorder', vid_res)
           # cv2.imshow('RJs WebCam Recorder', wC_frame)
